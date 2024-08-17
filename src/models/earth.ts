b8ba3ea4e214d5f7c1f27e7ea5a24c9e173403d1
Sayemahamed <sayemahamed183@gmail.com>
2024-08-17 22:40:39 +0600

    adding assets

--- src/models/earth.ts
+++ src/models/earth.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-const geometry = new THREE.IcosahedronGeometry(1, 1);
+const geometry = new THREE.IcosahedronGeometry(1, 12);
 const material = new THREE.MeshStandardMaterial({
   color: 0xffff00,
   flatShading: true,
