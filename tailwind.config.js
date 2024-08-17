b471e40b2e82246b700a0754ec47c149a05d62f5
Sayemahamed <sayemahamed183@gmail.com>
2024-08-17 20:31:13 +0600

    configuring for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
