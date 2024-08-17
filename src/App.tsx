22664da3de5879aea99cc06345876a6041bab073
Sayemahamed <sayemahamed183@gmail.com>
2024-08-17 21:44:39 +0600

    separated cube from main app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import * as THREE from "three";
-
+import cube from "./models/cube";
 function App() {
   useEffect(() => {
     const scene = new THREE.Scene();
@@ -10,15 +10,11 @@ function App() {
       0.1,
       1000
     );
-
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setAnimationLoop(animate);
     document.body.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
     camera.position.z = 5;
---
+++ src/models/cube.ts
@@ -0,0 +1,5 @@
+import * as THREE from "three";
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+export default cube;
