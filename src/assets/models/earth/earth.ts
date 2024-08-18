8f1c27555cbbf10a05b1442148f5e6fdf3efae0e
Sayemahamed <sayemahamed183@gmail.com>
2024-08-18 16:13:14 +0600

    upgraded mesh render

--- src/assets/models/earth/earth.ts
+++ src/assets/models/earth/earth.ts
@@ -1,10 +1,10 @@
 import * as THREE from "three";
-import earthMap from "./earthmap1k.jpg";
+import earthMap from "./earth.jpg";
 import earthBump from "./earthbump1k.jpg";
 import earthSpec from "./earthspec1k.jpg";
 import earthClouds from "./earthcloudmap.jpg";
 import earthCloudsTrans from "./earthcloudmaptrans.jpg";
-import earthLights from "./earthlights1k.jpg";
+import earthLights from "./earth_light.jpg";
 const loader = new THREE.TextureLoader();
 const geometry = new THREE.IcosahedronGeometry(1, 16);
 const material = new THREE.MeshPhongMaterial({
@@ -20,14 +20,14 @@ const lightsMat = new THREE.MeshBasicMaterial({
 const cloudsMat = new THREE.MeshStandardMaterial({
   map: loader.load(earthClouds),
   transparent: true,
-  opacity: 0.8,
+  opacity: 0.4,
   blending: THREE.AdditiveBlending,
   alphaMap: loader.load(earthCloudsTrans),
-  alphaTest: 0.3,
+  // alphaTest: 0.3,
 });
 const earthMesh = new THREE.Mesh(geometry, material);
 const lightsMesh = new THREE.Mesh(geometry, lightsMat);
 const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
-cloudsMesh.scale.setScalar(1.003);
+cloudsMesh.scale.setScalar(1.01);
 
 export default { earthMesh, cloudsMesh, lightsMesh };
