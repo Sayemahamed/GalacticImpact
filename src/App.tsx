0183c7df1545abb0c414188872d8029ea8b7632c
Sayemahamed <sayemahamed183@gmail.com>
2024-08-18 15:10:30 +0600

    added temporary earth

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import earth from "./models/earth";
+import getStarfield from "./models/stars";
 function App() {
   useEffect(() => {
     const scene = new THREE.Scene();
@@ -18,18 +19,35 @@ function App() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setAnimationLoop(animate);
     document.body.appendChild(renderer.domElement);
+    window.addEventListener("resize", handleWindowResize, false);
     new OrbitControls(camera, renderer.domElement);
     // <-- Add Elements -->
-    // scene.add(cube);
-    scene.add(earth);
+    //<< Earth Group >>
+    const earthGroup = new THREE.Group();
+    earthGroup.rotation.z = (-23.4 * Math.PI) / 180;
+    earthGroup.add(earth.earthMesh);
+    earthGroup.add(earth.lightsMesh);
+    earthGroup.add(earth.cloudsMesh);
+    scene.add(earthGroup);
+
+    // << Stars >>
+    const stars = getStarfield({ numStars: 3000 });
+    scene.add(stars);
     // <-- Add light Source -->
     const sunLight = new THREE.DirectionalLight(0xffffff, 2.0);
     sunLight.position.set(-2, 0.5, 1.5);
     scene.add(sunLight);
     function animate() {
-      earth.rotation.x += 0.01;
-      earth.rotation.y += 0.01;
+      earth.earthMesh.rotation.y += 0.002;
+      earth.lightsMesh.rotation.y += 0.002;
+      earth.cloudsMesh.rotation.y += 0.0023;
+      stars.rotation.y -= 0.0002;
       renderer.render(scene, camera);
+    }
+    function handleWindowResize() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
     }
   }, []);
   return <></>;
--- src/models/earth.ts
+++ src/models/earth.ts
@@ -1,9 +1,26 @@
 import * as THREE from "three";
+const loader = new THREE.TextureLoader();
 const geometry = new THREE.IcosahedronGeometry(1, 12);
-const material = new THREE.MeshStandardMaterial({
-  color: 0xffff00,
-  flatShading: true,
+const material = new THREE.MeshPhongMaterial({
+  map: loader.load("src/assets/textures/earth/earthmap1k.jpg"),
+  bumpMap: loader.load("src/assets/textures/earth/earthbump1k.jpg"),
+  specularMap: loader.load("src/assets/textures/earth/earthspec1k.jpg"),
+  bumpScale: 0.04,
 });
-const earth = new THREE.Mesh(geometry, material);
+const lightsMat = new THREE.MeshBasicMaterial({
+  map: loader.load("../assets/textures/earth/earthlights1k.jpg"),
+  blending: THREE.AdditiveBlending,
+});
+const cloudsMat = new THREE.MeshStandardMaterial({
+  map: loader.load("../assets/textures/earth/earthcloudmap.jpg"),
+  transparent: true,
+  opacity: 0.8,
+  blending: THREE.AdditiveBlending,
+  alphaMap: loader.load("../assets/textures/earth/earthcloudmaptrans.jpg"),
+  // alphaTest: 0.3,
+});
+const earthMesh = new THREE.Mesh(geometry, material);
+const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
 
-export default earth;
+export default { earthMesh, cloudsMesh, lightsMesh };
