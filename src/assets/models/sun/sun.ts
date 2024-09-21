ec4f08cbe27e9af5c62a6748ae88f072c31b453a
Sayemahamed <sayemahamed183@gmail.com>
2024-09-21 13:25:28 +0600

    grow effect added to sun

--- src/assets/models/sun/sun.ts
+++ src/assets/models/sun/sun.ts
@@ -1,7 +1,8 @@
 import * as THREE from "three";
 import sunMap from "./sun.jpg";
-import { getFresnelMat } from "./getFresnelMat";
 import { earthRotation, sunSize } from "../../Math/Constants";
+//@ts-ignore
+import FakeGlowMaterial from "./FakeGlowMaterial.js";
 
 const loader = new THREE.TextureLoader();
 const geometry = new THREE.IcosahedronGeometry(1, 12);
@@ -17,12 +18,12 @@ const material = new THREE.MeshPhongMaterial({
 
 const sunMesh = new THREE.PointLight(0xffffff, 6, 500, 0.05);
 sunMesh.add(new THREE.Mesh(geometry, material));
-const fresnelMat = getFresnelMat();
-const glowMesh = new THREE.Mesh(geometry, fresnelMat);
-glowMesh.scale.setScalar(1.002);
+const fakeGlowMaterial = new FakeGlowMaterial({ glowColor: 0xffffff });
+const radiationMesh = new THREE.Mesh(geometry, fakeGlowMaterial);
 const sun = new THREE.Group();
+radiationMesh.scale.setScalar(2);
 sun.add(sunMesh);
-sun.add(glowMesh);
+sun.add(radiationMesh);
 const update = () => {
   sun.rotation.y += earthRotation * (24 / 25);
 };
