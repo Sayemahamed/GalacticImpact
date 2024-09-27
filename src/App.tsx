41afb97cfccd48d3d8ef2d2508f4394340a1a6cf
Sayemahamed <sayemahamed183@gmail.com>
2024-09-27 22:14:10 +0600

    interactive name changing

--- src/App.tsx
+++ src/App.tsx
@@ -174,61 +174,66 @@ function App() {
       if (intersects.length > 0) {
         // An object was clicked
         const intersectedObject = intersects[0].object;
-        if (observationObject != intersectedObject) {
-          observationObject = intersectedObject;
-        }
+        if (intersectedObject == mercury.mercuryMesh) setID(1);
+        else if (intersectedObject == venus.venusMesh) setID(2);
+        else if (intersectedObject == earth.earthGroup) setID(3);
+        else if (intersectedObject == mars.marsMesh) setID(4);
+        else if (intersectedObject == jupiter.jupiterMesh) setID(5);
+        else if (intersectedObject == saturn.saturnMesh) setID(6);
+        else if (intersectedObject == uranus.uranusMesh) setID(7);
+        else if (intersectedObject == neptune.neptuneMesh) setID(8);
+        else if (intersectedObject == pluto.plutoMesh) setID(9);
+        else setID(0);
         console.log(position); // Logs the position in the console
       }
     }
   }, []);
   return (
-    <div className="absolute bg-transparent w-full h-full">
-      <div className="bg-transparent w-full h-12 p-3 grid justify-start md:justify-center ">
-        <div className="flex space-x-4 justify-center items-center">
-          <button
-            className="flex items-center justify-center shrink-0  w-10 h-10 rounded-full hover:color-white"
-            onClick={() =>
-              setID((x) => {
-                if (x > 0) return x - 1;
-                else return observationEntities.length - 1;
-              })
-            }
+    <div className="bg-transparent w-full h-12 p-3 grid justify-start md:justify-center absolute">
+      <div className="flex space-x-4 justify-center items-center">
+        <button
+          className="flex items-center justify-center shrink-0  w-10 h-10 rounded-full hover:color-white"
+          onClick={() =>
+            setID((x) => {
+              if (x > 0) return x - 1;
+              else return observationEntities.length - 1;
+            })
+          }
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-3 fill-gray-400"
+            viewBox="0 0 55.753 55.753"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-3 fill-gray-400"
-              viewBox="0 0 55.753 55.753"
-            >
-              <path
-                d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
-                data-original="#000000"
-              />
-            </svg>
-          </button>
-          <span className="text-xl w-16 text-center text-white font-bold">
-            {observationEntities[ID]}
-          </span>
-          <button
-            className="flex items-center justify-center shrink-0 ml-2  w-10 h-10 rounded-full hover:color-white"
-            onClick={() =>
-              setID((x) => {
-                if (x < observationEntities.length - 1) return x + 1;
-                else return 0;
-              })
-            }
+            <path
+              d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
+              data-original="#000000"
+            />
+          </svg>
+        </button>
+        <span className="text-xl w-16 text-center text-white font-bold">
+          {observationEntities[ID]}
+        </span>
+        <button
+          className="flex items-center justify-center shrink-0 ml-2  w-10 h-10 rounded-full hover:color-white"
+          onClick={() =>
+            setID((x) => {
+              if (x < observationEntities.length - 1) return x + 1;
+              else return 0;
+            })
+          }
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-3 fill-gray-400 rotate-180"
+            viewBox="0 0 55.753 55.753"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-3 fill-gray-400 rotate-180"
-              viewBox="0 0 55.753 55.753"
-            >
-              <path
-                d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
-                data-original="#000000"
-              />
-            </svg>
-          </button>
-        </div>
+            <path
+              d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
+              data-original="#000000"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
