950ab7d7c769c401747563a49cfda74ab6b9f52c
Sayemahamed <sayemahamed183@gmail.com>
2024-09-28 19:04:54 +0600

    correction in button

--- src/App.tsx
+++ src/App.tsx
@@ -225,17 +225,17 @@ function App() {
       <div className="flex space-x-4 justify-center items-center">
         <button
           className="flex items-center justify-center shrink-0  w-10 h-10 rounded-full hover:color-white"
-          id="increment"
-        >
+            id="decrement"
+          >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="w-3 fill-gray-400"
             viewBox="0 0 55.753 55.753"
-          >
+            >
             <path
               d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
               data-original="#000000"
-            />
+              />
           </svg>
         </button>
         <span className="text-xl w-16 text-center text-white font-bold">
@@ -243,7 +243,7 @@ function App() {
         </span>
         <button
           className="flex items-center justify-center shrink-0 ml-2  w-10 h-10 rounded-full hover:color-white"
-          id="decrement"
+          id="increment"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
