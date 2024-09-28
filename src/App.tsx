140c76b189b19457816923cf0749c6abbd292824
Sayemahamed <sayemahamed183@gmail.com>
2024-09-28 19:29:13 +0600

    added moon to the observation

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ function App() {
     "Mercury",
     "Venus",
     "Earth",
+    "Moon",
     "Mars",
     "Jupiter",
     "Saturn",
@@ -149,12 +150,14 @@ function App() {
       else if (objectId == 1) mercury.mercuryMesh.getWorldPosition(position);
       else if (objectId == 2) venus.venusMesh.getWorldPosition(position);
       else if (objectId == 3) earth.earthGroup.getWorldPosition(position);
-      else if (objectId == 4) mars.marsMesh.getWorldPosition(position);
-      else if (objectId == 5) jupiter.jupiterMesh.getWorldPosition(position);
-      else if (objectId == 6) saturn.saturnMesh.getWorldPosition(position);
-      else if (objectId == 7) uranus.uranusMesh.getWorldPosition(position);
-      else if (objectId == 8) neptune.neptuneMesh.getWorldPosition(position);
-      else if (objectId == 9) pluto.plutoMesh.getWorldPosition(position);
+      else if (objectId == 4)
+        earth.earthGroup.children[4].children[0].getWorldPosition(position);
+      else if (objectId == 5) mars.marsMesh.getWorldPosition(position);
+      else if (objectId == 6) jupiter.jupiterMesh.getWorldPosition(position);
+      else if (objectId == 7) saturn.saturnMesh.getWorldPosition(position);
+      else if (objectId == 8) uranus.uranusMesh.getWorldPosition(position);
+      else if (objectId == 9) neptune.neptuneMesh.getWorldPosition(position);
+      else if (objectId == 10) pluto.plutoMesh.getWorldPosition(position);
       control.target.set(position.x, position.y, position.z);
       control.update();
     }
@@ -202,18 +205,22 @@ function App() {
           updateObjectId(2);
         } else if (intersectedObject == earth.earthGroup) {
           updateObjectId(3);
-        } else if (intersectedObject == mars.marsMesh) {
+        } else if (
+          intersectedObject == earth.earthGroup.children[4].children[0]
+        ) {
           updateObjectId(4);
-        } else if (intersectedObject == jupiter.jupiterMesh) {
+        } else if (intersectedObject == mars.marsMesh) {
           updateObjectId(5);
-        } else if (intersectedObject == saturn.saturnMesh) {
+        } else if (intersectedObject == jupiter.jupiterMesh) {
           updateObjectId(6);
-        } else if (intersectedObject == uranus.uranusMesh) {
+        } else if (intersectedObject == saturn.saturnMesh) {
           updateObjectId(7);
-        } else if (intersectedObject == neptune.neptuneMesh) {
+        } else if (intersectedObject == uranus.uranusMesh) {
           updateObjectId(8);
-        } else if (intersectedObject == pluto.plutoMesh) {
+        } else if (intersectedObject == neptune.neptuneMesh) {
           updateObjectId(9);
+        } else if (intersectedObject == pluto.plutoMesh) {
+          updateObjectId(10);
         } else {
           updateObjectId(0);
         }
@@ -221,21 +228,21 @@ function App() {
     }
   }, []);
   return (
-    <div className="bg-transparent w-full h-12 p-3 grid justify-start md:justify-center absolute">
+    <div className="bg-transparent w-full h-12 p-3 grid justify-start md:justify-center absolute select-none">
       <div className="flex space-x-4 justify-center items-center">
         <button
           className="flex items-center justify-center shrink-0  w-10 h-10 rounded-full hover:color-white"
-            id="decrement"
-          >
+          id="decrement"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="w-3 fill-gray-400"
             viewBox="0 0 55.753 55.753"
-            >
+          >
             <path
               d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
               data-original="#000000"
-              />
+            />
           </svg>
         </button>
         <span className="text-xl w-16 text-center text-white font-bold">
