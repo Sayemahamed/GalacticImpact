d48d3bfe5d8f586f99d1ec4bec42def1d3e18416
Sayemahamed <sayemahamed183@gmail.com>
2024-10-01 20:37:45 +0600

    implementing information display

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,8 @@ import neptune from "./assets/models/neptune/neptune";
 import pluto from "./assets/models/pluto/pluto";
 import getStarfield from "./assets/models/stars/stars";
 import { earthCirculation, earthDistance } from "./assets/Math/Constants";
-
+import information from "../public/informations.json";
+import { div } from "three/webgpu";
 function App() {
   const observationEntities = [
     "Sun",
@@ -228,43 +229,61 @@ function App() {
     }
   }, []);
   return (
-    <div className="bg-transparent w-full h-12 p-3 grid justify-start md:justify-center absolute select-none">
-      <div className="flex space-x-4 justify-center items-center">
-        <button
-          className="flex items-center justify-center shrink-0  w-10 h-10 rounded-full hover:color-white"
-          id="decrement"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-3 fill-gray-400"
-            viewBox="0 0 55.753 55.753"
+    <>
+      <div className="bg-transparent w-full h-12 p-3 grid justify-start md:justify-center absolute select-none">
+        <div className="flex space-x-4 justify-center items-center">
+          <button
+            className="flex items-center justify-center shrink-0  w-10 h-10 rounded-full hover:color-white"
+            id="decrement"
           >
-            <path
-              d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
-              data-original="#000000"
-            />
-          </svg>
-        </button>
-        <span className="text-xl w-16 text-center text-white font-bold">
-          {observationEntities[ID]}
-        </span>
-        <button
-          className="flex items-center justify-center shrink-0 ml-2  w-10 h-10 rounded-full hover:color-white"
-          id="increment"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-3 fill-gray-400 rotate-180"
-            viewBox="0 0 55.753 55.753"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-3 fill-gray-400"
+              viewBox="0 0 55.753 55.753"
+            >
+              <path
+                d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
+                data-original="#000000"
+              />
+            </svg>
+          </button>
+          <span className="text-xl w-16 text-center text-white font-bold">
+            {observationEntities[ID]}
+          </span>
+          <button
+            className="flex items-center justify-center shrink-0 ml-2  w-10 h-10 rounded-full hover:color-white"
+            id="increment"
           >
-            <path
-              d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
-              data-original="#000000"
-            />
-          </svg>
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-3 fill-gray-400 rotate-180"
+              viewBox="0 0 55.753 55.753"
+            >
+              <path
+                d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
+                data-original="#000000"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
-    </div>
+      <div>
+        {information.map(
+          (info, index) =>
+            index === ID && (
+              <div className="absolute top-5 text-white right-0">
+                {Object.keys(info).map((key) => {
+                  return (
+                    <div>
+                      {key} : {info[key]}
+                    </div>
+                  );
+                })}
+              </div>
+            )
+        )}
+      </div>
+    </>
   );
 }
 
